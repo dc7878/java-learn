bd42ef7d913fa7ec4ad6ecc069f569bfae49ac75
dc7878 <1026961500@qq.com>
2017-12-13 22:40:10 +0800

    一些short的问题

---
+++ src/cc/dc/sort/TestDemo2.java
@@ -0,0 +1,13 @@
+package cc.dc.sort;
+
+/**
+ * short + int自动转成int
+ * short+=1;正常编译
+ */
+public class TestDemo2 {
+    public static void main(String[] args) {
+        short a = 1;
+        // a = a + 1;
+        a += 1;
+    }
+}
