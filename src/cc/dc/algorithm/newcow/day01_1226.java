388b0f432652e67324021b911bae5213ef23c2d0
dc7878 <1026961500@qq.com>
2017-12-26 20:54:37 +0800

    数组后续遍历

--- src/cc/dc/algorithm/newcow/day01_1226.java
+++ src/cc/dc/algorithm/newcow/day01_1226.java
@@ -1,7 +1,39 @@
 package cc.dc.algorithm.newcow;
 
+/**
+ * 已知一个数组，判断数组是不是二叉树的后续遍历的结果
+ */
 public class day01_1226 {
+
     public static void main(String[] args) {
-        System.out.println("1226");
+        int[] array = {1,4,3,5,6,7};
+        System.out.println(isLast(array));
+    }
+
+    private static boolean isLast(int[] array) {
+        if (array.length == 0) {
+            return false;
+        }
+        return isLastOrder(array, 0, array.length - 1);
+    }
+
+    private static boolean isLastOrder(int[] array, int start, int end) {
+        if (end <= start) {
+            return true;
+        }
+
+        int i = start;
+        for (; i < end; i++) {
+            if (array[i] > array[end]) {
+                break;
+            }
+        }
+        for (int j = i; j < end; j++) {
+            if (array[j] < array[end]) {
+                return false;
+            }
+        }
+
+        return isLastOrder(array, start, i - 1) && isLastOrder(array, i, end - 1);
     }
 }
