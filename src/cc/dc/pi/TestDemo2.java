ffc0044e6bf7cf5d22eabe68380a0c7f8e18a263
dc7878 <1026961500@qq.com>
2017-12-31 19:57:19 +0800

    一点提交

--- src/cc/dc/pi/TestDemo2.java
+++ src/cc/dc/pi/TestDemo2.java
@@ -3,6 +3,6 @@ package cc.dc.pi;
 public class TestDemo2 {
 
     public static void main(String[] args) {
-        System.out.println("774e59d3778771bd40d4ee4c0088e155".length());
+        System.out.println("14159265358979323846264338327950288419".length());
     }
 }
