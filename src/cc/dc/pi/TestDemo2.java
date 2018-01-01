4b4cb1b23d258394f8d4101cd6d45f3d61381a69
dc7878 <1026961500@qq.com>
2018-01-01 19:41:42 +0800

    目前的进度

--- src/cc/dc/pi/TestDemo2.java
+++ src/cc/dc/pi/TestDemo2.java
@@ -3,6 +3,6 @@ package cc.dc.pi;
 public class TestDemo2 {
 
     public static void main(String[] args) {
-        System.out.println("14159265358979323846264338327950288419".length());
+        System.out.println("1415926535897932384626433832795028841971693993751058209749445923".length());
     }
 }
